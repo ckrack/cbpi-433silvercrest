b37d35cbfbe5780f4b6530745e1c41cdd16f734b
Clemens Krack <info@clemenskrack.com>
2018-01-18 02:23:18 +0100

    Fix command

--- __init__.py
+++ __init__.py
@@ -10,15 +10,17 @@ class Silvercrest(ActorBase):
     b_offcode = Property.Text("Switch OFF code", configurable=True, default_value="2045856" )
 
     def send(self,  code):
+        r = ""
         try:
-            call(["sudo /home/clemens/433Utils/RPi_utils/./codesend", code, "4 355"])
+            call(["sudo /home/clemens/433Utils/RPi_utils/./codesend %s 4 355" % (code)], shell=True)
         except Exception as e:
-            self.api.app.logger.error("FAIELD to switch 433Mhz Code: %s" % (code))
+            self.api.app.logger.error("FAILED to switch 433Mhz Code: %s %s" % (code, r))
+        self.api.app.logger.error("SWITCHED 433Mhz Code: %s %s" % (code, r))
 
     def on(self, power=100):
-        self.send(self.onCommand)
+        self.send(self.a_oncode)
 
     def off(self):
-        self.send(self.onCommand)
+        self.send(self.b_offcode)
 
 
