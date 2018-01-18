d20ba67524525f2ac3180950bf61b27c8425e5b4
Clemens Krack <info@clemenskrack.com>
2018-01-18 22:55:39 +0100

    Optimize code and add new actor
    We have on actor that allows input of your own codes and
    we have another actor that uses a pre-defined list of codes.

--- __init__.py
+++ __init__.py
@@ -4,23 +4,55 @@ from modules.core.props import Property
 from subprocess import call
 
 @cbpi.actor
-class Silvercrest(ActorBase):
+class Silvercrest433Mhz(ActorBase):
 
-    a_oncode = Property.Text("Switch ON code", configurable=True, default_value="1125744" )
-    b_offcode = Property.Text("Switch OFF code", configurable=True, default_value="2045856" )
+    socket = Property.Select("socket", options=["A", "B", "C", "D"])
+
+    on_codes = {
+    	'A': '1045296',
+    	'B': '1045300',
+    	'C': '1045308',
+    	'D': '772370'
+    }
+
+    off_codes = {
+    	'A': '280608',
+    	'B': '230816',
+    	'C': '772380',
+    	'D': '1045298'
+    }
 
     def send(self,  code):
-        r = ""
         try:
-            call(["sudo /home/clemens/433Utils/RPi_utils/./codesend %s 4 355" % (code)], shell=True)
+            call(["sudo /home/pi/433Utils/RPi_utils/./codesend %s 4 355" % (code)], shell=True)
         except Exception as e:
-            self.api.app.logger.error("FAILED to switch 433Mhz Code: %s %s" % (code, r))
-        self.api.app.logger.error("SWITCHED 433Mhz Code: %s %s" % (code, r))
+            self.api.app.logger.error("FAILED to switch Silvercrest 433Mhz Code: %s" % (code))
 
     def on(self, power=100):
-        self.send(self.a_oncode)
+        self.send(self.on_codes[self.socket])
+        self.api.app.logger.info("SWITCHED Silvercrest 433Mhz Socket: %s - %s" % (self.socket, self.on_codes[self.socket]))
 
     def off(self):
-        self.send(self.b_offcode)
+        self.send(self.off_codes[self.socket])
+        self.api.app.logger.info("SWITCHED Silvercrest 433Mhz Socket: %s - %s" % (self.socket, self.off_codes[self.socket]))
 
+@cbpi.actor
+class SelfLearning433Mhz(ActorBase):
 
+    oncode = Property.Text("Switch ON code", configurable=True, default_value="")
+    offcode = Property.Text("Switch OFF code", configurable=True, default_value="")
+
+    def send(self,  code):
+        try:
+            call(["sudo /home/pi/433Utils/RPi_utils/./codesend %s 4 355" % (code)], shell=True)
+        except Exception as e:
+            self.api.app.logger.error("FAILED to switch 433Mhz Code: %s" % (code))
+        self.api.app.logger.info("SWITCHED Selflearning 433Mhz Code: %s" % (code))
+
+    def on(self, power=100):
+        self.send(self.oncode)
+        self.api.app.logger.info("SWITCHED Selflearning 433Mhz Socket: %s" % (self.oncode))
+
+    def off(self):
+        self.send(self.offcode)
+        self.api.app.logger.info("SWITCHED Selflearning 433Mhz Socket: %s" % (self.offcode))
